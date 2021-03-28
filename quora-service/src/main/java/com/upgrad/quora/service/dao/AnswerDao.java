8e0479efb2224efdda4faef485cd07e38e982973
prasjha <Prashant.Jha@amdocs.com>
2021-03-28 17:48:03 +0530

    Changes for Answer controller prt 3

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -1,28 +1,50 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.stereotype.Repository;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public class AnswerDao {
 
   @PersistenceContext private EntityManager entityManager;
 
-  //    public QuestionEntity createQuestion(QuestionEntity questionEntity) {
-  //        entityManager.persist(questionEntity);
-  //        return questionEntity;
-  //    }
+  public AnswerEntity createAnswer(AnswerEntity answerEntity) {
+    entityManager.persist(answerEntity);
+    return answerEntity;
+  }
 
-  //    public void updateUser(final UserEntity updatedUserEntity) {
-  //        entityManager.merge(updatedUserEntity);
-  //    }
-  //
-  //    public void deleteUser(final UserEntity deleteUser) {
-  //        entityManager.createNamedQuery("deleteUserAuthTokenByUserId",
-  // UserAuthTokenEntity.class).setParameter("userId",deleteUser.getId()).getSingleResult();
-  //        entityManager.createNamedQuery("deleteByUserName",
-  // UserEntity.class).setParameter("username",deleteUser.getUserName() ).getSingleResult();
-  //    }
+  public AnswerEntity getAnswerById(final String answerId) {
+    try {
+      return entityManager
+              .createNamedQuery("getAnswerById", AnswerEntity.class)
+              .setParameter("uuid", answerId)
+              .getSingleResult();
+    } catch (Exception nre) {
+      return null;
+    }
+  }
 
+  public void updateAnswer(AnswerEntity answerEntity) {
+    entityManager.merge(answerEntity);
+  }
+
+  public AnswerEntity deleteAnswer(final String answerId) {
+    AnswerEntity deleteAnswer = getAnswerById(answerId);
+    if (deleteAnswer != null) {
+      entityManager.remove(deleteAnswer);
+    }
+    return deleteAnswer;
+  }
+
+
+  public List<AnswerEntity> getAllAnswersToQuestion(final String questionId) {
+    return entityManager
+            .createNamedQuery("getAllAnswersToQuestion", AnswerEntity.class)
+            .setParameter("uuid", questionId)
+            .getResultList();
+  }
 }
